6d130dd70852176ceb0c2a25d0116e37035b97ee
angelo562 <username@example.com>
2023-10-06 09:00:33 -0700

    self.datelist

--- helper.py
+++ helper.py
@@ -11,7 +11,7 @@ def return_path():
 
     inp1 = r"C:\Users\Angel\Downloads\group_stats_detailed_164259_90sxeijc97.xlsx"
 
-    path1 = os.path.abspath(inp1.strip(' \"'))
+    path1 = os.path.abspath(inp1.strip(' "'))
     return fr"{path1}"
 
 
@@ -24,44 +24,52 @@ def get_thanksgiving_date(year):
     date += dt.timedelta(weeks=3)
     return date
 
+
 def get_thanksgiving_eve_date(year):
-    date = get_thanksgiving_date(year) 
+    date = get_thanksgiving_date(year)
     date -= dt.timedelta(days=1)
     return date
 
+
 def get_christmas_eve_date(year):
-    # Add year to 12-24 
+    # Add year to 12-24
     return dt.date(year, 12, 24)
+
 
 def get_christmas_date(year):
     # Add year to 12-25
-    return dt.date(year, 12,25)
+    return dt.date(year, 12, 25)
+
 
 def get_new_years_eve_date(year):
-    return dt.date(year, 12,31)
+    return dt.date(year, 12, 31)
+
 
 def get_new_years_date(year):
-    # returns the NEXT year 
+    # returns the NEXT year
     year += 1
     return dt.date(year, 1, 1)
-    
+
+
 def get_mothers_day_date(year):
 
     date = dt.date(year, 5, 1)
-    #first Sunday in may as 6
+    # first Sunday in may as 6
     while date.weekday() != 6:
         date += dt.timedelta(days=1)
     # add one week to get to 2nd sunday in may
     date += dt.timedelta(weeks=1)
     return date
 
+
 def get_halloween_date(year):
     return dt.date(year, 10, 31)
+
 
 minor_holiday_fxns = [
     get_mothers_day_date,
     get_halloween_date,
-    ]
+]
 
 major_holiday_fxns = [
     get_thanksgiving_date,
@@ -70,7 +78,7 @@ major_holiday_fxns = [
     get_christmas_date,
     get_new_years_eve_date,
     get_new_years_date,
-    ]
+]
 
 # collect what fxns to run
 def collect_fxns():
@@ -80,31 +88,33 @@ def collect_fxns():
     minor_h = True
 
     if major_h:
-        holiday_list_fxns +=  major_holiday_fxns
+        holiday_list_fxns += major_holiday_fxns
 
     if minor_h:
-        holiday_list_fxns +=  minor_holiday_fxns
-    
+        holiday_list_fxns += minor_holiday_fxns
+
     holiday_list_fxns = holiday_list_fxns
     return holiday_list_fxns
 
+
 def get_and_clean_df():
 
-    df = pd.read_excel(return_path() , header=1)
+    df = pd.read_excel(path, header=1)
 
-    df = df.query('not Date.isna()').reset_index(drop=True)  
+    df = df.query("not Date.isna()").reset_index(drop=True)
 
-    df = df.rename(columns={
-        'Work Hrs' : 'Work_hrs',
-        'Sched Hrs': 'Scheduled_hrs',
-        })
+    df = df.rename(columns={"Work Hrs": "Work_hrs", "Sched Hrs": "Scheduled_hrs",})
 
-    #change Date to datetime dtype
-    df.Date = pd.to_datetime(df.Date)       
-    
-    #Ensure df.Time is a str
-    df['Time'] = df['Time'].astype(str)     
-    #Format Provider names to be title()
-    df.Provider = df.Provider.str.title().astype(str)    
+    # change Date to datetime dtype
+    df.Date = pd.to_datetime(df.Date)
+
+    # Ensure df.Time is a str
+    df["Time"] = df["Time"].astype(str)
+    # Format Provider names to be title()
+    df.Provider = df.Provider.str.title().astype(str)
 
     return df
+
+
+
+path = return_path()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
+
 # import os
 
 from datetime import timedelta as td
 import helper
-
 
 
 """Shift Admin Buddy.  Designed to give schedule stats
@@ -21,52 +21,51 @@ Include Major holidays, minor holidays, weekends, nights
 
 """
 
-class ShiftAdminBuddy:
 
+class ShiftAdminBuddy:
     def __init__(self):
         self.holidays = True
         self.weekends = False
-        self.path = helper.return_path()
-        # self.datelist = self.get_datelist_asstring()
+        self.path = helper.path
+        self.datelist = self.get_datelist_asstring()
         # self.df = self.get_and_clean_df()
         # self.df = self.compile_df_to_analyze()
         # self.fxn_list = self.collect_fxns()
         # self.beg_date = self.df.Date.min().strftime('%Y-%m-%d')
         # self.end_date = (self.df.Date.max()+ td(days=1)).strftime('%Y-%m-%d')
-  
-
 
     def compile_df_to_analyze(self):
-
         def concat_df(df_1, df_2):
-            return  pd.concat([df_1, df_2], axis= 0)
+            return pd.concat([df_1, df_2], axis=0)
 
         def query_df(date):
             date = date
-            return self.df.query('Date == @date')
+            return self.df.query("Date == @date")
 
         df = query_df(self.datelist[0])
         for date in self.datelist[1:]:
             df = concat_df(df, query_df(date))
         return df
-    
-    def get_datelist_asstring(self, fxn_list= None, year= 2022):
+
+    def get_datelist_asstring(self, fxn_list=None, year=2022):
         if self.holidays:
             if fxn_list is None:
-                fxn_list = self.collect_fxns()
+                fxn_list = helper.collect_fxns()
 
-            date_list_asstring=[]
+            date_list_asstring = []
             for fxn in fxn_list:
-                date_list_asstring.append(fxn(self,year).strftime('%Y-%m-%d'))
-            return date_list_asstring
-
+                date_list_asstring.append(fxn(year).strftime("%Y-%m-%d"))
+            return sorted(date_list_asstring)
+        
     def count_holidays(self):
         self.holidays = True
         self.weekends = False
 
-        df_c = self.df.sort_values(by='Provider')\
-        .reset_index(drop=True)\
-        .Provider.value_counts()
+        df_c = (
+            self.df.sort_values(by="Provider")
+            .reset_index(drop=True)
+            .Provider.value_counts()
+        )
 
         print(df_c)
         return df_c
@@ -75,9 +74,9 @@ class ShiftAdminBuddy:
         self.holidays = False
         self.weekends = True
 
-        
 
 sab = ShiftAdminBuddy()
+print(sab.datelist)
 
 # "C:\Users\Angel\Downloads\group_stats_detailed_164259_90sxeijc97.xlsx"
 # print(sab.path)
